07f3eac2334ddd452ef99b4274550993dfbec9e0
Lychee030 <61603017+Lychee030@users.noreply.github.com>
2021-12-29 19:17:18 -0500

    bot sends encourage message

--- encourageBot.py
+++ encourageBot.py
@@ -1,11 +1,21 @@
 import discord
 from dotenv import load_dotenv
 import os
+import requests
+import json
 
 load_dotenv()
 
 '''connection to discord'''
 client=discord.Client()
+
+def get_quote(): # return a quote from api
+    response = requests.get("https://zenquotes.io/api/random")
+    # convert response to json
+    json_data = json.loads(response.text)
+    # q stands for quote; a stands for author
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return quote
 
 # register an event
 @client.event
@@ -16,8 +26,11 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    '''if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')'''
+    if message.content.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
         
 TOKEN = os.getenv('TOKEN')
 client.run(TOKEN)
